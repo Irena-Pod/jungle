d94ffa7852f9a998932dddd98a64e8542b9dd73d
Irena-Pod <irena.podgurski@gmail.com>
2021-02-03 23:48:37 +0000

    add admin security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user, password|
+      user == ENV["ADMIN_USERNAME"] && password == ENV["ADMIN_PASSWORD"]
+    end
+  end
+
   private
 
   def cart
